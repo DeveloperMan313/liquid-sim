a4715658bfe93f9c0339bbef1bd31e909b2d25d9
DeveloperMan313 <053000voloshch@gmail.com>
2026-02-02 23:29:57 +0300

    Move rendering to render_liquid()

--- src/render.rs
+++ src/render.rs
@@ -13,13 +13,11 @@ pub async fn run() {
 
     let dt = 0.1;
 
-    let texture_size = Vec2 {
-        x: (conf.grid_width * conf.cell_size_px) as f32,
-        y: (conf.grid_height * conf.cell_size_px) as f32,
-    };
-
     // TODO remove this
-    request_new_screen_size(texture_size.x, texture_size.y);
+    request_new_screen_size(
+        (conf.grid_width * conf.cell_size_px) as f32,
+        (conf.grid_height * conf.cell_size_px) as f32,
+    );
 
     // for demo
     for y in 1..grid_1.len() - 1 {
@@ -38,57 +36,64 @@ pub async fn run() {
     }
 
     loop {
-        // simulate frame
         simulate_frame(&mut grid_1, &mut grid_2, dt, &conf);
 
-        // render frame
-        let texture_bytes: Vec<u8> = grid_1
-            .iter()
-            .flatten()
-            .map(|cell: &Cell| match conf.render_view {
-                RenderView::Color => cell.color.to_array(),
-                RenderView::Speed => {
-                    let speed = cell.vel.length();
-                    let speed_0_to_1 = speed / (speed + 1.0);
-                    (DVec4::ONE * speed_0_to_1).to_array()
-                }
-                RenderView::Velocity => {
-                    let vel_minus_1_to_1 = cell.vel / (cell.vel.length() + 1.0);
-                    dvec4(
-                        vel_minus_1_to_1.x.abs(),
-                        vel_minus_1_to_1.y.abs(),
-                        vel_minus_1_to_1.dot(dvec2(1.0, 1.0)),
-                        1.0,
-                    )
-                    .to_array()
-                }
-            })
-            .flatten()
-            .map(|f: f64| (f * 256.) as u8)
-            .collect();
-
-        let texture = Texture2D::from_rgba8(
-            conf.grid_width as u16,
-            conf.grid_height as u16,
-            &texture_bytes,
-        );
-        texture.set_filter(FilterMode::Nearest);
-
-        draw_texture_ex(
-            &texture,
-            0.,
-            0.,
-            WHITE,
-            DrawTextureParams {
-                dest_size: Some(texture_size),
-                source: None,
-                rotation: 0.,
-                flip_x: false,
-                flip_y: false,
-                pivot: None,
-            },
-        );
+        render_liquid(&grid_1, &conf);
 
         next_frame().await
     }
+}
+
+fn render_liquid(grid: &Vec<Vec<Cell>>, conf: &SimConfig) {
+    let texture_bytes: Vec<u8> = grid
+        .iter()
+        .flatten()
+        .map(|cell: &Cell| match conf.render_view {
+            RenderView::Color => cell.color.to_array(),
+            RenderView::Speed => {
+                let speed = cell.vel.length();
+                let speed_0_to_1 = speed / (speed + 1.0);
+                (DVec4::ONE * speed_0_to_1).to_array()
+            }
+            RenderView::Velocity => {
+                let vel_minus_1_to_1 = cell.vel / (cell.vel.length() + 1.0);
+                dvec4(
+                    vel_minus_1_to_1.x.abs(),
+                    vel_minus_1_to_1.y.abs(),
+                    vel_minus_1_to_1.dot(dvec2(1.0, 1.0)),
+                    1.0,
+                )
+                .to_array()
+            }
+        })
+        .flatten()
+        .map(|f: f64| (f * 256.) as u8)
+        .collect();
+
+    let texture = Texture2D::from_rgba8(
+        conf.grid_width as u16,
+        conf.grid_height as u16,
+        &texture_bytes,
+    );
+    texture.set_filter(FilterMode::Nearest);
+
+    let texture_size = Vec2 {
+        x: (conf.grid_width * conf.cell_size_px) as f32,
+        y: (conf.grid_height * conf.cell_size_px) as f32,
+    };
+
+    draw_texture_ex(
+        &texture,
+        0.,
+        0.,
+        WHITE,
+        DrawTextureParams {
+            dest_size: Some(texture_size),
+            source: None,
+            rotation: 0.,
+            flip_x: false,
+            flip_y: false,
+            pivot: None,
+        },
+    );
 }
